82142a31687daa69e1c42decd3ebfe2b15846294
DerivFX4 <derivfx4@gmail.com>
2026-09-20 22:36:01 +0300

    Add VintelTool Studio rsbuild.config.ts

---
+++ rsbuild.config.ts
@@ -0,0 +1,1 @@
+import { defineConfig } from '@rsbuild/core'; import { pluginReact } from '@rsbuild/plugin-react'; export default defineConfig({plugins:[pluginReact()],source:{entry:{index:'./src/main.tsx'}},output:{distPath:{root:'dist'}},html:{template:'./index.html'}});
